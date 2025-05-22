a5b6346e0fdfe71d75a47b9f8a582a212b49cfb6
anuragtiwari3100 <anuragtiwarigpj2005@gmail.con>
2025-05-22 12:49:53 +0530

    Add core JPA entity classes for e-commerce domain model

---
+++ src/main/java/com/tiwari/model/PaymentDetails.java
@@ -0,0 +1,21 @@
+package com.tiwari.model;
+import com.tiwari.domain.PaymentStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentDetails {
+
+	private String paymentId;
+	private String razorpayPaymentLinkId;
+	private String razorpayPaymentLinkReferenceId;
+	private String razorpayPaymentLinkStatus;
+	private String razorpayPaymentId​;
+	private PaymentStatus status;
+
+
+
+}
